5175bf44813e25a6f732c53cc4506e8acb48bb34
Jasper Wang <wangdc2010@gmail.com>
2018-08-17 14:18:44 -0400

    Use line renderer instead of trail renderer for better rendering

--- Assets/Scripts/Draw.cs
+++ Assets/Scripts/Draw.cs
@@ -5,23 +5,22 @@ public class Draw : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-        
     }
 	
 	// Update is called once per frame
 	void Update () {
         if (shouldUpdate && ((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved) || Input.GetMouseButton(0)))
         {
-            double relativeX = (Input.mousePosition.x / Screen.width - 0.5) * 0.25;
-            double relativeZ = Input.mousePosition.y / Screen.height - 0.5;
-            Debug.Log("x: " + relativeX + ", y: " + this.transform.position.y + ", z: " + this.transform.position.z);
+            double relativeX = (Input.mousePosition.x / Screen.width - 0.5) * -10;
+            double relativeZ = (Input.mousePosition.y / Screen.height - 0.5) * -10;
+            Debug.Log("x: " + relativeX + ", z: " + relativeZ);
 
-            this.transform.position = new Vector3((float)relativeX, this.transform.position.y, (float)relativeZ);
+            this.transform.localPosition = new Vector3((float)relativeX, 0, (float)relativeZ);
+            this.transform.localRotation = Quaternion.identity;
         }
         else if ((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended) || Input.GetMouseButtonUp(0))
         {
             shouldUpdate = false;
-            this.transform.parent = null;
         }
         
     }
--- Assets/Scripts/DrawingManager.cs
+++ Assets/Scripts/DrawingManager.cs
@@ -18,12 +18,32 @@ public class DrawingManager : MonoBehaviour {
         if ((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) || Input.GetMouseButtonDown(0))
         {
             Debug.Log("Start drawing");
-            double relativeX = (Input.mousePosition.x / Screen.width - 0.5) * 0.25;
+            double relativeX = Input.mousePosition.x / Screen.width - 0.5;
             double relativeZ = Input.mousePosition.y / Screen.height - 0.5;
-            startPos = new Vector3((float)relativeX, gameObject.transform.position.y, (float)relativeZ);
+            startPos = new Vector3((float)relativeX, 0, (float)relativeZ);
+
+            /*
+            // this creates a horizontal plane passing through this object's center
+            var plane = new Plane(canvas.transform.position, Vector3.up);
+            // create a ray from the mousePosition
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            // plane.Raycast returns the distance from the ray start to the hit point
+            float distance;
+            if (plane.Raycast(ray, out distance))
+            {
+                // some point of the plane was hit - get its coordinates
+                startPos = ray.GetPoint(distance);
+                // use the hitPoint to aim your cannon
+            }
+            */
             thisTrail = Instantiate(drawingPrefab, startPos, Quaternion.identity) as GameObject;
             thisTrail.transform.parent = gameObject.transform;
-            thisTrail.transform.eulerAngles = new Vector3(0,0,0);
+
+            var localTrailRenderer = canvas.AddComponent<TrailRenderer_Local>();
+            localTrailRenderer.objToFollow = thisTrail.transform;
+            localTrailRenderer.limitTrailLength = false;
+            localTrailRenderer.maxPositions = 1000;
+            localTrailRenderer.distIncrement = 0.1F;
         }
     }
 }
